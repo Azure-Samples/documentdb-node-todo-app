792535f8cb9fab02aa83556ce6d9982c62be1e91
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2018-06-25 14:10:49 -0500

    Revert "Updating Jade to pug"
    This reverts commit 0312fd364b3fc342e8e2debd87e204b719ae1fb6.

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
